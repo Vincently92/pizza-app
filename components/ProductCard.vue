17b6ce20d2f1f008df3817322db1e1bca72041f2
Leonid Bugaenko <vincently@mail.ru>
2022-03-13 16:45:02 +0300

    change images path

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,27 +1,39 @@
 <template>
-  <v-card max-width="344" max-height="460px" class="float-left ma-4 py-2 px-2" elevation=4>
-
-    <v-img class="ml-4 " :src="pizza.image"></v-img>
+  <v-card
+    max-width="344"
+    max-height="460px"
+    class="float-left ma-4 py-2 px-2"
+    elevation="4"
+  >
+    <v-img class="ml-4 " :src="imagesUrl + pizza.image"></v-img>
 
     <v-card-title>
-      {{pizza.name}}
+      {{ pizza.name }}
     </v-card-title>
 
     <v-card-subtitle style="max-height:3em;overflow:hidden;">
-      {{pizza.description}}
+      {{ pizza.description }}
     </v-card-subtitle>
 
     <v-card-actions>
       <div class="text-h5 px-2">
-        {{ pizza.price[this.$store.getters['currency/currency']] }}
-        {{this.$store.getters['currency/currenciesSymbol'][this.$store.getters['currency/currency']]}}
+        {{ pizza.price[this.$store.getters["currency/currency"]] }}
+        {{
+          this.$store.getters["currency/currenciesSymbol"][
+            this.$store.getters["currency/currency"]
+          ]
+        }}
       </div>
       <v-spacer></v-spacer>
       <div>
-        <v-btn-toggle v-if="this.$store.getters['cart/cart'][pizza.id]" dense multiple>
+        <v-btn-toggle
+          v-if="this.$store.getters['cart/cart'][pizza.id]"
+          dense
+          multiple
+        >
           <v-btn @click="decrease(pizza)" style="min-width:40px">-</v-btn>
           <v-btn outlined color="red" class="px-2" to="/cart">
-            {{this.$store.getters['cart/cart'][pizza.id].quantity}} in cart
+            {{ this.$store.getters["cart/cart"][pizza.id].quantity }} in cart
           </v-btn>
           <v-btn @click="increase(pizza)" style="min-width:40px">+</v-btn>
         </v-btn-toggle>
@@ -30,24 +42,28 @@
         </v-btn>
       </div>
     </v-card-actions>
-
   </v-card>
 </template>
 
 <script>
-import { mapMutations } from 'vuex'
+import { mapMutations } from "vuex";
 export default {
-  props:{
-    pizza:{
+  data() {
+    return {
+      imagesUrl: process.env.IMAGES_URL
+    };
+  },
+  props: {
+    pizza: {
       type: Object
     }
   },
-  methods:{
+  methods: {
     ...mapMutations({
-        decrease: 'cart/decrease',
-        increase: 'cart/increase',
-        add: 'cart/add'
+      decrease: "cart/decrease",
+      increase: "cart/increase",
+      add: "cart/add"
     })
   }
-}
+};
 </script>
--- pages/orders.vue
+++ pages/orders.vue
@@ -2,67 +2,95 @@
   <div>
     <v-expansion-panels>
       <v-expansion-panel v-for="order of orders" :key="order.id">
-
         <v-expansion-panel-header>
-          <v-col cols="6">Order #{{order.id}}</v-col>
-          <v-col cols="6">{{formatDate(order.created_at)}}</v-col>
-          <v-col cols="6">{{order.city}} {{order.address}}</v-col>
-          <v-col cols="6">{{order.total.toFixed(2)}} {{$store.getters['currency/currenciesSymbol'][order.currency]}}</v-col>
+          <v-col cols="6">Order #{{ order.id }}</v-col>
+          <v-col cols="6">{{ formatDate(order.created_at) }}</v-col>
+          <v-col cols="6">{{ order.city }} {{ order.address }}</v-col>
+          <v-col cols="6"
+            >{{ order.total.toFixed(2) }}
+            {{
+              $store.getters["currency/currenciesSymbol"][order.currency]
+            }}</v-col
+          >
         </v-expansion-panel-header>
 
         <v-expansion-panel-content>
-
-          <v-list-item threeLine v-for="purchase in order.purchases" :key="purchase.id">
+          <v-list-item
+            threeLine
+            v-for="purchase in order.purchases"
+            :key="purchase.id"
+          >
             <v-list-item-avatar height="100" width="100" class="hidden-xs-only">
-              <v-img :src="purchase.product.image"></v-img>
+              <v-img :src="imagesUrl + purchase.product.image"></v-img>
             </v-list-item-avatar>
 
             <v-list-item-content>
-              <v-list-item-title v-text="purchase.product.name" class="text-lg-h6 py-2"></v-list-item-title>
-              <v-list-item-subtitle v-text="purchase.product.description"></v-list-item-subtitle>
+              <v-list-item-title
+                v-text="purchase.product.name"
+                class="text-lg-h6 py-2"
+              ></v-list-item-title>
+              <v-list-item-subtitle
+                v-text="purchase.product.description"
+              ></v-list-item-subtitle>
             </v-list-item-content>
 
-            <v-list-item-content class="mx-auto text-center d-sm-none d-md-flex">
+            <v-list-item-content
+              class="mx-auto text-center d-sm-none d-md-flex"
+            >
               <div>
-                {{ purchase.price }} {{$store.getters['currency/currenciesSymbol'][order.currency]}}
+                {{ purchase.price }}
+                {{
+                  $store.getters["currency/currenciesSymbol"][order.currency]
+                }}
               </div>
             </v-list-item-content>
 
             <v-list-item-content class="text-center" style="max-width:110px;">
-              <v-text-field class="mx-auto text-center" style="height:50px;" :value="purchase.quantity" readonly solo
-                flat type="text"></v-text-field>
+              <v-text-field
+                class="mx-auto text-center"
+                style="height:50px;"
+                :value="purchase.quantity"
+                readonly
+                solo
+                flat
+                type="text"
+              ></v-text-field>
             </v-list-item-content>
 
             <v-list-item-content class="mx-auto text-center">
               <div>
                 {{ (purchase.price * purchase.quantity).toFixed(2) }}
-                {{$store.getters['currency/currenciesSymbol'][order.currency]}}
+                {{
+                  $store.getters["currency/currenciesSymbol"][order.currency]
+                }}
               </div>
             </v-list-item-content>
           </v-list-item>
         </v-expansion-panel-content>
-
       </v-expansion-panel>
     </v-expansion-panels>
-
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import moment from 'moment'
-  export default {
-    data: () => ({
-      orders: {}
-    }),
-    async mounted() {
-        this.orders = await this.$axios.$get( process.env.BACKEND_URL + '/orders', {headers: {'Authorization': `Bearer ${this.$store.getters['user/accessToken']}`}})
-    },
-    methods: {
-      formatDate(value){
-        return moment(String(value)).format('MM/DD/YYYY hh:mm')
+import { mapState } from "vuex";
+import moment from "moment";
+export default {
+  data: () => ({
+    imagesUrl: process.env.IMAGES_URL,
+    orders: {}
+  }),
+  async mounted() {
+    this.orders = await this.$axios.$get(process.env.BACKEND_URL + "/orders", {
+      headers: {
+        Authorization: `Bearer ${this.$store.getters["user/accessToken"]}`
       }
+    });
+  },
+  methods: {
+    formatDate(value) {
+      return moment(String(value)).format("MM/DD/YYYY hh:mm");
     }
   }
-
+};
 </script>
